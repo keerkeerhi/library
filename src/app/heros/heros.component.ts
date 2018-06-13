11577d5305a6d55b3ecd7e9bd466d8a4251c77d9
mvlyforever <123278392@qq.com>
2018-06-13 20:12:57 +0800

    leaning over

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heros',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-  heros = 'aaa'
-  constructor() { }
+  heros = []
+  selectedHero
+  constructor(private hService:HeroService,private mService:MessageService) { }
 
   ngOnInit() {
+    console.log('=---==',this.hService)
+    this.hService.getHeros().subscribe(res=>{
+      // this.mService.add('data heros geted')
+      this.heros = res;
+    })
   }
 
+  showHero(h){
+    this.selectedHero = h
+    this.mService.add(h.id+h.name)
+    console.log('===>',h)
+  }
 }
