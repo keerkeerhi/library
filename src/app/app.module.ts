dc4c41997dcb35d0d914439144f25f7be18c359b
zhenghx <123278392@qq.com>
2018-06-18 19:45:54 +0800

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { AppComponent } from './app.component';
 // module
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
 
 @NgModule({
   declarations: [
@@ -17,9 +25,13 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
-  providers: [],
+  providers: [{
+    provide: SWIPER_CONFIG,
+    useValue: DEFAULT_SWIPER_CONFIG
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  config = {
+    autoplay: {
+      delay: 5000
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      bulletClass: 'page-index',
+      bulletActiveClass: 'page-index-acv'
+    },
+  }
+
+  windowH = 0;
+
+  banners = []
+
+  navList = []
+  constructor() {
+    this.windowH = this.getClientHeight();
+    this.banners = [
+      {img:"/assets/img/back1.jpg"},
+      {img:"/assets/img/back2.jpg"},
+      {img:"/assets/img/back3.jpg"},
+      {img:"/assets/img/back4.jpg"}]
+
+    this.navList = [
+      {title:'首页'},
+      {title:'概况'},
+      {title:'党建'},
+      {title:'古迹'},
+      {title:'留言板'}
+    ]
+  }
+
+  getClientHeight() {
+    var clientHeight = 0;
+    if (document.body.clientHeight && document.documentElement.clientHeight) {
+      var clientHeight = (document.body.clientHeight < document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+    }
+    else {
+      var clientHeight = (document.body.clientHeight > document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+    }
+    return clientHeight;
+  }
 
   ngOnInit() {
+
   }
 
 }
